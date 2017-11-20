9322b8def916da70d182f6bc625fcffad8b37de0
Lumi <olivier.tinh@gmail.com>
2017-11-20 12:11:12 +0100

    Début de fenêtre

--- Crypto 5 ENSISA/src/application/Main.java
+++ Crypto 5 ENSISA/src/application/Main.java
@@ -1,9 +1,19 @@
 package application;
 
+import javax.swing.*;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("LUMI IS TESTING");
+		System.out.println("LUMI IS TESTING SOME GOOD SHIT RIGHT HERE");
+
+		JFrame window = new JFrame();
+
+		window.setTitle("WEEWOO WEEWOO");
+		window.setSize(400, 200);
+		window.setLocationRelativeTo(null);
+		window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		window.setVisible(true);
 	}
 
 }
