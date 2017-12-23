04b42580a0277dd6883d6e74a8c1e9861448ab55
Lumi <olivier.tinh@gmail.com>
2017-12-23 04:17:36 +0100

    Quick test

--- Crypto 5 ENSISA/src/application/Main.java
+++ Crypto 5 ENSISA/src/application/Main.java
@@ -5,8 +5,6 @@ import javax.swing.*;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("LUMI IS TESTING SOME GOOD SHIT RIGHT HERE");
-
 		JFrame window = new JFrame();
 
 		window.setTitle("WEEWOO WEEWOO");
