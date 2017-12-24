e09e718b4cc735f318b69426e745cffa11f4de4e
Lumi <olivier.tinh@gmail.com>
2017-12-24 03:34:57 +0100

    Frame + panel + open image

--- Crypto 5 ENSISA/src/application/Frame.java
+++ Crypto 5 ENSISA/src/application/Frame.java
@@ -1,23 +1,80 @@
 package application;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.io.File;
 
-public class Frame extends JFrame {
+public class Frame extends JFrame implements ActionListener {
 
     private final JMenuBar MENU_BAR = new JMenuBar();
+    private final JMenu FICHIER_MENU = new JMenu();
+    private final JMenuItem OUVRIR_MENU = new JMenuItem();
+    private final JMenuItem FERMER_MENU = new JMenuItem();
+    private final Panel panel = new Panel();
+
+    private FileNameExtensionFilter extensionFilter;
 
     public Frame() {
         super();
+        setFrameProperties();
+        createMenuBar();
+        extensionFilter = new FileNameExtensionFilter(".jpg, .jpeg, .png", "jpg", "jpeg", "png");
+    }
+
+    private void setFrameProperties() {
         this.setTitle("Super projet de cryptographie");
         this.setSize(400, 200);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
-        createMenu();
     }
 
-    private void createMenu() {
+    private void resizeFrame() {
+        this.setSize(panel.image.getWidth() + 16, panel.image.getHeight() + 62);
+        this.setLocationRelativeTo(null);
+    }
+
+    private void createMenuBar() {
         setJMenuBar(MENU_BAR);
+
+        createMenu(FICHIER_MENU, "Fichier");
+        createSubMenu(OUVRIR_MENU, "Ouvrir", FICHIER_MENU);
+        createSubMenu(FERMER_MENU, "Fermer", FICHIER_MENU);
+
+        getContentPane().add(panel);
     }
 
+    private void createMenu(JMenu menu, String name) {
+        MENU_BAR.add(menu);
+        menu.setText(name);
+    }
+
+    private void createSubMenu(JMenuItem subMenu, String name, JMenu menu) {
+        menu.add(subMenu);
+        subMenu.addActionListener(this);
+        subMenu.setText(name);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(OUVRIR_MENU)) {
+            openFile();
+        } else if (e.getSource().equals(FERMER_MENU)) {
+            exitFile();
+        }
+    }
+
+    private void openFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(extensionFilter);
+        if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION)
+            panel.addImage(new File(fileChooser.getSelectedFile().getAbsolutePath()));
+        resizeFrame();
+    }
+
+    private void exitFile() {
+        this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
+    }
 }
--- Crypto 5 ENSISA/src/application/Panel.java
+++ Crypto 5 ENSISA/src/application/Panel.java
@@ -1,14 +1,33 @@
 package application;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Panel extends JPanel {
 
-    BufferedImage image = null;
+    protected BufferedImage image = null;
 
     public Panel() {
         super();
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (image != null)
+            g.drawImage(image, 0, 0, null);
+    }
+
+    protected void addImage(File imageFile) {
+        try {
+            image = ImageIO.read(imageFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        repaint();
     }
 
 }
