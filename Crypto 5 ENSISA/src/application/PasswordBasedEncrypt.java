27e3f657d8fa3a141a1259c00fdec09567b9e3b9
Bastien Geldreich <bastien.geldreich@free.fr>
2017-12-28 14:40:11 +0100

    Utilisation de l'API cryptographique Java+génération Cipher utilisable

--- Crypto 5 ENSISA/src/application/PasswordBasedEncrypt.java
+++ Crypto 5 ENSISA/src/application/PasswordBasedEncrypt.java
@@ -1,85 +1,58 @@
 package application;
 
-import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 
 public class PasswordBasedEncrypt {
 	//Quand on veut decrypter, avec le même mdp on doit avoir la meme cle
-	int c = 64; //iterations
-	byte[] salt;
-	int lK; //length Key -> fixee du coup 
+	public int c; //iterations
+	public byte[] salt;
+	public PBEParameterSpec pSpecs;
 	
-	/**
-	 * @param a
-	 * @param b
-	 * @return the ceil of the division a/b
-	 */
-	int ceil(int a,int b){
-		if (a%b==0){
-			return a/b;
-		} else {
-			return a/b+1;
-		}
+	public PasswordBasedEncrypt(int c,byte[] salt){
+		this.salt= salt;
+		this.c=c;
+		pSpecs = new PBEParameterSpec(salt, c);
 	}
 	
 	/**
-	 * @param a
-	 * @param b
-	 * @return the result of the xor operation between a and b
+	 * @param password
+	 * @return une SecretKey générée à partir du mot de passe
 	 */
-	byte[] xor(byte[] a,byte[] b){
-		byte[] result= new byte[a.length];
-		for(int i=0;i<a.length;i++){
-			result[i]= (byte) (a[i]^b[i]);
-		}
-		return result;
-	}
-	
-	byte[] encrypt(byte[] a,byte[] key){ //mettre le cryptage ici ou remplacer par une autre fonction
-		return a;
-	}
-	
-	/**
-	 * @param salt
-	 * @param indice
-	 * @return salt+(byte[])indice
-	 */
-	byte[] append(byte[] salt,int indice){
-		byte[] fin= BigInteger.valueOf(indice).toByteArray();
-		byte[] result= new byte[salt.length+4];
-		for(int i=0;i<salt.length;i++){
-			result[i]=salt[i];
-		}
-		for(int i=0;i<4;i++){
-			result[salt.length+i]=fin[i];
-		}
-		return result;
+	SecretKey fromPassword(char[] password){
+		SecretKey key= null;
+		try {
+			SecretKeyFactory keyFact = SecretKeyFactory.getInstance("PBEWithMD5AndDES");
+			PBEKeySpec kSpecs = new PBEKeySpec(password);
+			key = keyFact.generateSecret(kSpecs); //On crée la clé secrète
+		} catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return key;
 	}
 	
 	/**
-	 * @param pwd
-	 * @return an encrypted key made from the password with the PBKDF2 method
+	 * @param password
+	 * @return une instance de Cipher qui nous permettra de crypter notre image
 	 */
-	public String encryptPBKDF2(String pwd){
-		byte[] password = pwd.getBytes();
-		int hL=10; //a changer //longueur de la chaine générée par notre fonction d'encryption
-		
-		int l = ceil(lK,hL);
-		int r = lK-hL*(l-1);
-		String K="";
-		
-		byte[] U;
-		byte[] A;
-		for(int i=1;i<=l;i++){
-			U = encrypt(password,append(salt,i)); // on appliquera la fonction de cryptage ici
-			A = U;
-			for(int j=1;j<=c-1;j++){
-				U = encrypt(password,U);//Rajouter la fonction appliquee sur U avec le password
-				A= xor(A,U);
-			}
-			K=K+A.toString();
+	Cipher getCipher(char[] password){
+		Cipher cipher=null;
+		try {
+			cipher = Cipher.getInstance("PBEWithMD5AndDES");
+			cipher.init(Cipher.ENCRYPT_MODE, fromPassword(password), pSpecs);
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e) {
+			e.printStackTrace();
 		}
-		
-		return K;
+		return cipher;
 	}
-
 }
