59646e97177a82566ac2cf03f4dbf9a893f59510
Anakin <Anakin@192.168.1.12>
2017-12-30 23:22:23 +0100

    Retrait de la classe ScrollBar ; ajout d'un canvas permetant l'utilisation correcte des scrollbars

--- Crypto 5 ENSISA/src/application/EncryptorModel.java
+++ Crypto 5 ENSISA/src/application/EncryptorModel.java
@@ -2,43 +2,34 @@ package application;
 
 import javax.swing.*;
 
-import components.ScrollBar;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.util.Random;
 import javax.imageio.ImageIO;
 
 public class EncryptorModel extends JPanel {
 
-	
-	private ScrollBar horizontal = new ScrollBar(this , "Horizontal");
-	private ScrollBar vertical = new ScrollBar(this , "Vertical");
 	private static final long serialVersionUID = -7000404362747523378L;
-
 	private BufferedImage image = null;
     private Point p1 = null;
     private Point p2 = null;
-    private Rectangle selectionRectangle;
+    private JPanel canvas;
 
     EncryptorModel() {
         super();
-        setLayout(new BorderLayout());
-        add(horizontal, BorderLayout.SOUTH);
-        add(vertical, BorderLayout.EAST);
-       
-    }
+        this.canvas = new JPanel() {
+			private static final long serialVersionUID = 1L;
 
-    /* --- Image --- */
+			protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.drawImage(image, 0, 0, null);
+            }
+        };
+    }
 
     public BufferedImage getImage() {
         return image;
-    }
-
-    public Rectangle getSelectionRectangle() {
-        return selectionRectangle;
     }
 
     protected void paintComponent(Graphics g) {
@@ -47,23 +38,27 @@ public class EncryptorModel extends JPanel {
         final Graphics2D g2 = (Graphics2D) g;
         drawImage(g2);
         drawSelectionRectangle(g2);
+        
   }
 
     private void drawImage(Graphics2D g2) {
-        if (image != null)
+        if (image != null){
             g2.drawImage(image, 0, 0, image.getWidth(), image.getHeight(), this);
+	        canvas.setPreferredSize(new Dimension(image.getWidth(), image.getHeight()));
+	        JScrollPane sp = new JScrollPane(canvas);
+	        setLayout(new BorderLayout());
+	        add(sp, BorderLayout.CENTER);
+        }
     }
 
     private void drawSelectionRectangle(Graphics2D g2) {
-        if (p2 == null || p1 == null) {
-            selectionRectangle = null;
+        if (p2 == null || p1 == null)
             return;
-        }
 
         g2.setColor(Color.BLACK);
 
         // calcul de la selection
-        selectionRectangle = new Rectangle((p2.x > p1.x) ? p1.x : p2.x,
+        final Rectangle rect = new Rectangle((p2.x > p1.x) ? p1.x : p2.x,
                 (p2.y > p1.y) ? p1.y : p2.y,
                 (p2.x > p1.x) ? p2.x - p1.x : p1.x
                         - p2.x, (p2.y > p1.y) ? p2.y
@@ -71,11 +66,11 @@ public class EncryptorModel extends JPanel {
 
         // dessine le fond de la selection avec un effet de transparence
         g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .2f));
-        g2.fillRect(selectionRectangle.x, selectionRectangle.y, selectionRectangle.width, selectionRectangle.height);
+        g2.fillRect(rect.x, rect.y, rect.width, rect.height);
 
         // suppression de la transparence pour dessiner la bordure
         g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
-        g2.drawRect(selectionRectangle.x, selectionRectangle.y, selectionRectangle.width, selectionRectangle.height);
+        g2.drawRect(rect.x, rect.y, rect.width, rect.height);
     }
 
     protected void addImage(File imageFile) {
@@ -83,7 +78,7 @@ public class EncryptorModel extends JPanel {
             image = ImageIO.read(imageFile);
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        } 
         repaint();
     }
 
@@ -92,55 +87,4 @@ public class EncryptorModel extends JPanel {
         this.p2 = p2;
         repaint();
     }
-
-    /* --- Encryption --- */
-
-    public void encryptionTest() {
-        // A supprimer après, juste du test
-        String imageToString;
-
-        imageToString = getRGBToString();
-        modifyThisShit();
-
-        System.out.println(": " + imageToString);
-        repaint();
-    }
-
-    /**
-     * Permet d'obtenir les valeurs RGB du rectangle sélectionné dans l'image sous forme de String
-     *
-     * @return valeurs RGB sous forme de String
-     */
-    private String getRGBToString() {
-        StringBuilder result = new StringBuilder();
-
-        for (int i = 0; i < image.getWidth(); i++) {
-            for (int j = 0; j < image.getHeight(); j++) {
-                if (selectionRectangle.contains(new Point(i,j))) {
-                    result.append(String.valueOf(image.getRGB(i, j) + "|"));
-                }
-            }
-        }
-
-        return result.toString();
-    }
-
-    private void modifyThisShit() {
-        Random rand = new Random();
-
-        for (int i = 0; i < image.getWidth(); i++) {
-            for (int j = 0; j < image.getHeight(); j++) {
-                if (selectionRectangle.contains(new Point(i,j))) {
-                    int alpha = 255;
-                    int red = rand.nextInt(255);
-                    int green = rand.nextInt(255);
-                    int blue = rand.nextInt(255);
-                    int p = (alpha << 24) | (red << 16) | (green << 8) | blue;
-                    image.setRGB(i, j, p);
-                }
-            }
-        }
-
-    }
-
 }
