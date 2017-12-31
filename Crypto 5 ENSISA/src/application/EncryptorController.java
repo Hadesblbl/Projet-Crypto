c1ae10befb40d76978b1e7bed9a6f4cb352c4588
olivier <olivier.tinh@gmail.com>
2017-12-31 18:13:29 +0100

    Fixed: import

--- Crypto 5 ENSISA/src/application/EncryptorController.java
+++ Crypto 5 ENSISA/src/application/EncryptorController.java
@@ -1,14 +1,11 @@
 package application;
 
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-
 import encryption.CryptedImage;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
