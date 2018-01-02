e37c1f34c0986827cc9512ee4e4dc1908bf962cb
Bastien Geldreich <bastien.geldreich@free.fr>
2018-01-02 17:09:08 +0100

    refix du bouton annuler crypter decrypter

--- Crypto 5 ENSISA/src/application/PopUp.java
+++ Crypto 5 ENSISA/src/application/PopUp.java
@@ -7,12 +7,17 @@ public class PopUp {
 	 * Identification mot de passe
 	 * return password
 	 */
-	static char[] PopupIdentification() {
+	static char[] PopupIdentification(){
 		String reponse = "";
-		while (reponse.equals(""))
+		while (reponse.equals("")){
 			reponse = JOptionPane.showInputDialog(null,"Veuillez entrer votre mot de passe","Identification",JOptionPane.QUESTION_MESSAGE);
+			if (reponse==null){//Bouton Annuler
+				return null;
+			}
+		}
 		JOptionPane.showMessageDialog(null, "Votre mot de passe est " + reponse, "Identité", JOptionPane.INFORMATION_MESSAGE);
 		return reponse.toCharArray();
+		
 	}
 }
 
